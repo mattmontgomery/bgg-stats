93f58c88880819c3f019780cd7da9d09e1b50ea0
Matt Montgomery <montgomery.matt@gmail.com>
2019-01-28 17:30:33 -0700

    Updates

---
+++ precache-manifest.f9097b21dc81ea29ce413b0a20dc3c31.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9061fa9166f9142481fd",
+    "url": "/games/static/js/runtime~main.9061fa91.js"
+  },
+  {
+    "revision": "69256f31057895c316f8",
+    "url": "/games/static/js/main.69256f31.chunk.js"
+  },
+  {
+    "revision": "ad8d873bd1fca44b7608",
+    "url": "/games/static/js/1.ad8d873b.chunk.js"
+  },
+  {
+    "revision": "69256f31057895c316f8",
+    "url": "/games/static/css/main.6ed5bd00.chunk.css"
+  },
+  {
+    "revision": "d6da4dbff55be2a40f28a9b63a309b45",
+    "url": "/games/index.html"
+  }
+];
